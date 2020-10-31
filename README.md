563825b2697d243e2d61cf7d07d9dbb61fd7265e
swo <scott@scottolesen.com>
2020-10-31 08:54:39 -0400

    prototype

--- README.md
+++ README.md
@@ -1,6 +1,12 @@
 # My vimrc
 
+## Getting started
+
+- Put `source /path/to/this/repo/.vimrc` into your `~/.vimrc`
+
 ## To do
 
-- Consider "sourcing" this `.vimrc`, rather than symlinking
+- Make a script that automatically makes the global `.vimrc`
+- Or, just symlink it?
 - Consider symlinking `start/`, so that the cloned repos live here, rather than under `~/.vim/`
+- Make it so that the target path isn't hard-coded
--- download-packages.py
+++ download-packages.py
@@ -1,9 +1,20 @@
 #!/usr/bin/env python3
 
-import json, subprocess
+import json, os.path, subprocess
 
 # read in list of packages
+with open("package_list.json") as f:
+    package_list = json.load(f)
 
 # check which ones already exist
+start_path = "/home/swo/.vim/pack/swo/start"
+existing_packages = os.listdir(start_path)
+needed_packages = [x for x in package_list if x not in existing_packages]
+
+print(needed_packages)
 
 # clone the missing ones
+for package in needed_packages:
+    target = os.path.join(start_path, package)
+    command = ["gh", "repo", "clone", package_list[package], target]
+    subprocess.run(command)
